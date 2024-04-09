b3f95a1b4eba645bed462de8dde5ee5cfb143b80
rohidekiever <rodrigo.hideki@evermart.com.br>
2024-04-09 12:17:41 -0300

    add swagger

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json'); 
+
 const router = require("./interfaces/routes/routes");
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use("/api", router);
 app.listen(3000, () => {
     require("../src/infrastructure/databases/sequelize");
